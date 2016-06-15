1d7c60196f123547bfb1193cfa6ff3c4b9fe025b
mauro <mauroparis96@gmail.com>
2016-06-15 13:10:25 -0300

    profile model -Rest

--- app_users/models.py
+++ app_users/models.py
@@ -1,23 +1,14 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
-# 
-# class Profile(models.Model):
-#
-#     first_name
-#     last_name
-#     dni
-#     age
-#
-#
-# class Employee(models.Model):
-#
-#     profile = models.ForeignKey(Profile, related_name = "employee")
-#
-#
-# class Doctor(models.Model):
-#
-#     employee = models.ForeignKey(Employee, related_name = "doctor")
-#     field
+
+class Profile(models.Model):
+    def __init__(self, dni,birth_date,adress,phone_num, enroll_date):
+        self.dni = dni
+        self.birth_date = birth_date
+        self.adress = adress
+        self.phone_num = phone_num
+        self.enroll_date = enroll_date
+        
