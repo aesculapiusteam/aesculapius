1e1392fb8c463a39e9582d3ea6971b6d50ce0ec8
mateosss <mateodemayo@gmail.com>
2016-06-15 13:58:17 -0300

    Models branch

--- app_users/models.py
+++ app_users/models.py
@@ -5,10 +5,19 @@ from datetime import datetime
 # Create your models here.
 
 class Profile(models.Model):
-    def __init__(self, dni,birth_date,adress,phone_num, enroll_date):
+    dni = models.IntegerField()
+    birth_date = models.DateField()
+    adress = models.CharField()
+    phone_num = models.CharField()
+    enroll_date = models.CharField()
+    def __init__(self, dni, birth_date, adress, phone_num, enroll_date):
         self.dni = dni
         self.birth_date = birth_date
         self.adress = adress
         self.phone_num = phone_num
         self.enroll_date = enroll_date
-        
+
+class Doctor(models.Model):
+    profile = models.ForeignKey(Profile, related_name = "doctor")
+    def __init__(self, profile)
+        self.profile = Profile.objects.get(pk=profile)
