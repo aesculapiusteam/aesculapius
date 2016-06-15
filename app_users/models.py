b53a0c0d01ff736de1e7f9c782ea8f76b47ffcdd
mateosss <mateodemayo@gmail.com>
2016-06-15 14:22:49 -0300

    Some initial user models

--- app_users/models.py
+++ app_users/models.py
@@ -7,15 +7,16 @@ from datetime import datetime
 class Profile(models.Model):
     dni = models.IntegerField()
     birth_date = models.DateField()
-    adress = models.CharField()
-    phone_num = models.CharField()
-    enroll_date = models.CharField()
-    def __init__(self, dni, birth_date, adress, phone_num, enroll_date):
+    adress = models.CharField(max_length=256)
+    phone_num = models.CharField(max_length=50)
+    enroll_date = models.DateField()
+    
+    def __init__(self, dni, birth_date, adress, phone_num):
         self.dni = dni
         self.birth_date = birth_date
         self.adress = adress
         self.phone_num = phone_num
-        self.enroll_date = enroll_date
+        self.enroll_date = datetime.now()
 
 class Doctor(models.Model):
     profile = models.ForeignKey(Profile, related_name = "doctor")
