c3759143113e744ef8b1ede64f4fb724c126fb10
mateosss <mateodemayo@gmail.com>
2016-06-16 13:59:04 -0300

    Updated models

--- app_users/models.py
+++ app_users/models.py
@@ -1,38 +1,50 @@
 from __future__ import unicode_literals
 from django.db import models
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.fields import GenericRelation
-from django.contrib.contenttypes.models import ContentType
+# from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+# from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User, UserManager
 from datetime import datetime
 
-
 class Profile(models.Model):
-    dni = models.IntegerField()
-    birth_date = models.DateField()
-    adress = models.CharField(max_length=256)
-    phone_number = models.CharField(max_length=50)
-    create_date = models.DateField()
+    first_name = models.CharField(max_length=128, null=True)
+    last_name = models.CharField(max_length=128, null=True)
+    email = models.CharField(max_length=128, null=True)
 
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
-    user = GenericForeignKey("content_type", "object_id")
+    dni = models.IntegerField(null=True)
+    birth_date = models.DateField(null=True)
+    adress = models.CharField(max_length=256, null=True)
+    phone = models.CharField(max_length=50, null=True)
+    cellphone = models.CharField(max_length=50, null=True)
+    creation_date = models.DateField(auto_now_add = True)
 
-    def save(self, *args, **kwargs):
-        self.create_date = datetime.now()
-        super(Profile, self).save(*args, **kwargs)
+    # content_type = models.ForeignKey(ContentType, null=True)
+    # object_id = models.PositiveIntegerField(null=True)
+    # user = GenericForeignKey("content_type", "object_id")
 
-class Pacient(models.Model):
-    profile = GenericRelation('Profile')
-    clinic_history = None #TODO
+    # def save(self, *args, **kwargs):
+    #     self.creation_date = datetime.now()
+    #     super(Profile, self).save(*args, **kwargs)
 
-class Employee(models.Model):
+class Pacient(Profile):
+    #Es una clase separada por que va a tener funciones especiales como getHC
+    # profile = GenericRelation('Profile', related_query_name='profile')
+    def __str__():
+        return "Pacient: " + this.name
+
+class Employee(Profile):
     #TODO Importar de django auth
     user = models.CharField(max_length=128)
     password = models.CharField(max_length=128)
-    profile = GenericRelation('Profile')
+    def __str__():
+        return "Pacient: " + this.name
+    # profile = GenericRelation('Profile', related_query_name='profile')
 
-class Doctor(models.Model):
-    horario = None # TODO
+class Doctor(Employee):
+    hours = None # TODO
 
-class Secretary(models.Model):
+class Secretary(Employee):
     doctors = models.ManyToManyField(Doctor, related_name="doctor")
+
+# paciente = Pacient()
+# paciente.profile.create(dni=40987366, adress="Duarte quiros 5647")
+# paciente.save()
