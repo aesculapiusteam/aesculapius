effe5d63b7f97e3a2265b708e858f20671cff287
mateosss <mateodemayo@gmail.com>
2016-06-16 11:55:04 -0300

    mauro de merd

--- app_users/models.py
+++ app_users/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
 from datetime import datetime
 
@@ -14,31 +15,21 @@ class Profile(models.Model):
 
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
-    user = GenericForeignKey("user_type", "user_id")
-    content_object = generic.GenericForeignKey()
+    user = generic.GenericForeignKey("content_type", "object_id")
 
     def save(self, *args, **kwargs):
         self.create_date = datetime.now()
         super(Profile, self).save(*args, **kwargs)
 
-class Comment(models.Model):#TODO
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
-    content_object = generic.GenericForeignKey()
-
-class Post(models.Model):
-  comments = generic.GenericRelation('Comment')
-
-class Picture(models.Model):
-  comments = generic.GenericRelation('Comment')
-
 class Pacient(models.Model):
+    profile = generic.GenericRelation('Profile')
     clinic_history = None #TODO
 
 class Employee(models.Model):
     #TODO Importar de django auth
     user = models.CharField(max_length=128)
     password = models.CharField(max_length=128)
+    profile = generic.GenericRelation('Profile')
 
 class Doctor(models.Model):
     horario = None # TODO
