20aad2888e9c2363d8415c9b39e281428fa669fc
RenzoSartore <sartorerenzo@gmail.com>
2016-06-26 16:24:02 -0300

    Added info and history app in pacient

--- static/js/init.js
+++ static/js/init.js
@@ -3,6 +3,7 @@
 
     $('.button-collapse').sideNav();
     $('.modal-trigger').leanModal();
+    $('.fixed-action-btn').openFAB();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
