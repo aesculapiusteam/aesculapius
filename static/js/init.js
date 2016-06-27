53b57fd74404867e6d499f99c1299c8759c0708a
garciamarianoCBA <garciamariano47@gmail.com>
2016-06-27 13:48:48 -0300

    favicon

--- static/js/init.js
+++ static/js/init.js
@@ -1,9 +1,14 @@
 (function($){
-  $(function(){
+    $(function(){
 
-    $('.button-collapse').sideNav();
-    $('.modal-trigger').leanModal();
-    $('.fixed-action-btn').openFAB();
-  }); // end of document ready
+        $('.button-collapse').sideNav();
+        $('.modal-trigger').leanModal();
+        $('.fixed-action-btn').openFAB();
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
 
+
+$(document).ready(function(){
+    $('.slider').slider({full_width: true});
+    $('.slider').slider({indicators: false, height: 574});
+});
