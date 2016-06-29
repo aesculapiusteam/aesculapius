1dfb2b04ef3091a4727b18f697b50a90eb333933
mateosss <mateodemayo@gmail.com>
2016-06-29 20:35:20 -0300

    Fixed details

--- aesculapius/urls.py
+++ aesculapius/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('app_users.urls', namespace="api")),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^', include('app_main.urls')),
+    url(r'^', include('app_main.urls', namespace='main')),
 ]
