de969485f5e241e6533ecf615617037660190f3b
garciamarianoCBA <garciamariano47@gmail.com>
2016-06-29 12:54:32 -0300

    slider home

--- static/js/init.js
+++ static/js/init.js
@@ -4,11 +4,17 @@
         $('.button-collapse').sideNav();
         $('.modal-trigger').leanModal();
         $('.fixed-action-btn').openFAB();
+        var h = window.innerHeight - 64;
+        $('.slider').slider({
+            full_width: true,
+            height: h,
+            indicators: false
+        });
     }); // end of document ready
 })(jQuery); // end of jQuery name space
 
 
-$(document).ready(function(){
-    $('.slider').slider({full_width: true});
-});
+/*$(document).ready(function(){
+
+});*/
 
