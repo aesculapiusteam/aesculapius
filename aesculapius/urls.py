f4ddc2b9de4e842200f9b0c249ba61a117a48eb5
mateosss <mateodemayo@gmail.com>
2016-11-01 14:11:29 -0300

    Home url redirects to /api/

--- aesculapius/urls.py
+++ aesculapius/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework.authtoken import views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('api.urls', namespace='api')),
     url(r'^api/session-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/token-auth/', views.obtain_auth_token),
+    url(r'^', RedirectView.as_view(url='/api/'), name='home'),
 ]
